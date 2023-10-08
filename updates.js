568fead66f665850c38ebcb00620cd70c7bf8178
webratte <webratte@users.noreply.github.com>
2023-10-08 08:27:51 +0200

    Revert "import only updates"...
    because it was a really stupid solution 😫

--- updates.js
+++ updates.js
@@ -1,19 +1,5 @@
 function update() {
     let promise = window.webxdc.setUpdateListener((update) => {
-// begin handling "read only newest update"
-      readUnixtime();
-      if (window.storedSentTime === null) {
-        window.storedSentTime = 0;
-      };
-
-      window.CurrentTime = update.payload.CurrentTime;
-      if (window.CurrentTime === undefined) {
-        window.CurrentTime = 1;
-      };
-
-      if (window.CurrentTime >= window.storedSentTime) {
-        storeIncomingTime();
-// end handling "read only newest update"
         arrHistory = update.payload.arrHistory;
         serial= update.serial;
         max_serial = update.max_serial;
@@ -23,34 +9,14 @@ function update() {
               SilentMsg();
           fillTables();
         };
-      }
     }, );
 };
 
    function sendUpdate() {
-// begin handling "read only newest update"
-    storeSentTime();
-// end handling "read only newest update"
     window.webxdc.sendUpdate({
       payload: {
-      arrHistory, CurrentTime,
+      arrHistory,
       },
       info,
      }, info);
    };
-
-// store time if send-button is pressed
-function storeSentTime() {
-  window.CurrentTime = Date.now();
-  localStorage.setItem('sentUnixTime', window.CurrentTime);
-};
-
-// store time of incoming update
-function storeIncomingTime() {
-  localStorage.setItem('sentUnixTime', window.CurrentTime);	
-};
-
-// read time of already exist update
-function readUnixtime() {
-  window.storedSentTime = localStorage.getItem('sentUnixTime');
-};
